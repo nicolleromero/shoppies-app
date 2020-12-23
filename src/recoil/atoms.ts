a4c4887a969edcc7234b5e97c1381bbb64b0d63c
Nicolle Romero <nicollearomero@gmail.com>
2020-12-23 14:19:35 -0800

    Added local storage

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,61 +1,31 @@
 import { atom } from 'recoil';
 
 import { Item } from '../utils/omdb';
+import { localStorageEffect } from './effects';
 
-// type PersistenceOptions<T>: {
-//   key: string,
-//   restorer: (mixed, DefaultValue) => T | DefaultValue,
-// };
-
-// const currentUserIDState = atom<number>({
-//   key: 'CurrentUserID',
-//   default: 1,
-//   effects_UNSTABLE: [
-//     localStorageEffect({
-//       key: 'current_user',
-//       restorer: (value, defaultValue) =>
-//         // values are currently persisted as numbers
-//         typeof value === 'number'
-//           ? value
-//           : // if value was previously persisted as a string, parse it to a number
-//           typeof value === 'string'
-//           ? parseInt(value, 10)
-//           : // if type of value is not recognized, then use the atom's default value.
-//             defaultValue,
-//     }),
-//   ],
-// });
-
-const inputState = atom<string>({
+export const inputState = atom<string>({
   key: 'inputState',
   default: '',
 });
 
-const movieListState = atom<Item[]>({
+export const movieListState = atom<Item[]>({
   key: 'movieListState',
   default: [],
 });
 
-const nominationListState = atom<Item[]>({
+export const nominationListState = atom<Item[]>({
   key: 'nominationListState',
   default: [],
+  effects_UNSTABLE: [localStorageEffect('nominationListState')],
 });
 
-const searchState = atom<string>({
+export const searchState = atom<string>({
   key: 'searchState',
   default: '',
+  effects_UNSTABLE: [localStorageEffect('searchState')],
 });
 
-const selectedState = atom({
+export const selectedState = atom({
   key: 'selectedState',
   default: [],
 });
-
-export {
-  // currentUserIDState,
-  inputState,
-  movieListState,
-  nominationListState,
-  searchState,
-  selectedState,
-};
