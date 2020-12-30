9005936dba38f06cf08fc0fff7567002df03b1f9
Nicolle Romero <nicollearomero@gmail.com>
2020-12-30 13:05:29 -0800

    Added MoviePage component

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
 
-import { searchState } from '../recoil/atoms';
-import { filteredSearchQuery } from '../recoil/selectors';
-import { Movie } from './Movie';
+import { useSearchTerm } from '../utils/hooks';
+import { MoviePage } from './MoviePage';
 
 import './MovieList.css';
 
 export function MovieList() {
-  const results = useRecoilValue(filteredSearchQuery(1));
-  const searchTerm = useRecoilValue(searchState);
-
-  if (!searchTerm && !results.length) {
-    return null;
-  }
+  const query = useSearchTerm();
 
   return (
     <div className="grid-container" role="list">
-      {results.length ? (
-        results.map((item) => {
-          return <Movie item={item} key={item.id} />;
-        })
-      ) : (
-        <p className="no-results-message">
-          We weren't able to find any movies that match that title. Please modify your search and
-          try again.
-        </p>
-      )}
+      <MoviePage page={1} query={query} />;
     </div>
   );
 }
---
+++ src/components/MoviePage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+
+import { filteredSearchQuery } from '../recoil/selectors';
+import { MovieCard } from './MovieCard';
+
+import './MovieList.css';
+
+type Props = {
+  query: string;
+  page: number;
+};
+
+export function MoviePage(props: Props) {
+  const results = useRecoilValue(filteredSearchQuery(props));
+
+  console.log('result', results);
+  if (!props.query && !results.length) {
+    return null;
+  }
+
+  return (
+    <>
+      {results.length ? (
+        results.map((movie) => {
+          return <MovieCard movie={movie} key={movie.id} />;
+        })
+      ) : props.page === 1 ? (
+        <p className="no-results-message">
+          We weren't able to find any movies that match that title. Please modify your search and
+          try again.
+        </p>
+      ) : null}
+    </>
+  );
+}
