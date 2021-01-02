df80de6bb3b6180fd029b6f4bd78f618b8035e9e
Nicolle Romero <nicollearomero@gmail.com>
2021-01-02 15:15:08 -0800

    Added MovieDetails component

--- src/components/MovieDetailsModal.tsx
+++ src/components/MovieDetailsModal.tsx
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { omdbMovieDetails } from '../recoil/selectors';
+import { nominationListState } from '../recoil/atoms';
 // import { DismissButton } from './DismissButton';
+import { NominateButton } from './NominateButton';
 import { Poster } from './Poster';
 
-// import './MovieDetailsModal.css';
+import './MovieDetailsModal.css';
 
 type Props = {
   match: any;
@@ -22,21 +24,46 @@ export function MovieDetailsModal(props: Props) {
 function MovieDetailsModalContent(props: Props) {
   const movieId = props.match.params.id;
   const movie = useRecoilValue(omdbMovieDetails(movieId));
+  const nominationsList = useRecoilValue(nominationListState);
+  const nominated = nominationsList.some((obj) => obj.id === movieId);
 
   if (!movie) {
     return null;
   }
 
   function handleCloseModal() {
-    <></>;
+    // TODO: navigate to /?q=search
   }
 
   return (
-    <div className="movie-details-modal">
+    <div className="modal">
+      {nominated && (
+        <div className="ribbon ribbon-top-right">
+          <span>NOMINATED</span>
+        </div>
+      )}
       {/* <DismissButton movie={movie} onClick={handleCloseModal} /> */}
       <Poster movie={movie}></Poster>
+      <NominateButton movie={movie} />
       <div className="movie-details-title">
-        {movie.title} ({movie.year})
+        <div>
+          {movie.title} <span className="year">({movie.year})</span>
+        </div>
+        <div className="supporting-details">
+          {movie.rated} | {movie.runtime} | {movie.genre} | {movie.released}
+        </div>
+        <div className="plot">
+          Summary: {movie.plot}
+          <p>
+            <strong>Director:</strong> {movie.director}
+          </p>
+          <p>
+            <strong>Writer:</strong> {movie.writer}
+          </p>
+          <p>
+            <strong>Actors:</strong> {movie.actors}
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -45,3 +72,8 @@ function MovieDetailsModalContent(props: Props) {
 function MovieDetailsModalFallback() {
   return null;
 }
+
+// ratings: MovieRating[];
+// metascore: string;
+// imdbRating: string;
+// imdbVotes: string;
