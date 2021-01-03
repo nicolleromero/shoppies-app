1b7074e17bc47cfec49e023501e8614e877fc8a2
Nicolle Romero <nicollearomero@gmail.com>
2021-01-03 13:35:07 -0800

    Added ratings to movie details

--- src/components/MovieDetailsModal.tsx
+++ src/components/MovieDetailsModal.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
+import { Redirect } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { omdbMovieDetails } from '../recoil/selectors';
 import { nominationListState } from '../recoil/atoms';
-// import { DismissButton } from './DismissButton';
+import { DismissButton } from './DismissButton';
+import { ErrorBoundary } from './ErrorBoundary';
 import { NominateButton } from './NominateButton';
 import { Poster } from './Poster';
 
@@ -15,9 +17,11 @@ type Props = {
 
 export function MovieDetailsModal(props: Props) {
   return (
-    <Suspense fallback={<MovieDetailsModalFallback />}>
-      <MovieDetailsModalContent {...props} />
-    </Suspense>
+    <ErrorBoundary fallback={<Redirect to="/" />}>
+      <Suspense fallback={<MovieDetailsModalFallback />}>
+        <MovieDetailsModalContent {...props} />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
@@ -31,9 +35,14 @@ function MovieDetailsModalContent(props: Props) {
     return null;
   }
 
-  function handleCloseModal() {
-    // TODO: navigate to /?q=search
-  }
+  const metascore = movie.ratings[2].Value.slice(0, 2);
+  console.log(metascore);
+
+  const rottenTomatoes = Number(movie.ratings[1].Value.slice(0, 2));
+  console.log(rottenTomatoes);
+
+  const imdb = movie.ratings[0].Value.slice(0, 3);
+  console.log(imdb);
 
   return (
     <div className="modal">
@@ -42,27 +51,61 @@ function MovieDetailsModalContent(props: Props) {
           <span>NOMINATED</span>
         </div>
       )}
-      {/* <DismissButton movie={movie} onClick={handleCloseModal} /> */}
-      <Poster movie={movie}></Poster>
-      <NominateButton movie={movie} />
-      <div className="movie-details-title">
+      <div className="column">
+        <DismissButton />
+        <Poster clickable={false} movie={movie}></Poster>
+        <NominateButton movie={movie} />
+      </div>
+      <div className="column">
+        <div className="movie-details">
+          <h2 className="title">
+            {movie.title} <span className="year">({movie.year})</span>
+          </h2>
+          <div className="supporting-details">
+            {movie.rated} | {movie.runtime} | {movie.genre} | {movie.released}
+          </div>
+        </div>
         <div>
-          {movie.title} <span className="year">({movie.year})</span>
-        </div>
-        <div className="supporting-details">
-          {movie.rated} | {movie.runtime} | {movie.genre} | {movie.released}
-        </div>
-        <div className="plot">
-          Summary: {movie.plot}
-          <p>
-            <strong>Director:</strong> {movie.director}
-          </p>
-          <p>
-            <strong>Writer:</strong> {movie.writer}
-          </p>
-          <p>
-            <strong>Actors:</strong> {movie.actors}
-          </p>
+          <div className="plot">
+            <p>
+              <strong>Summary:</strong> {movie.plot}
+            </p>
+            <p>
+              <strong>Director:</strong> {movie.director}
+            </p>
+            <p>
+              <strong>Writer:</strong> {movie.writer}
+            </p>
+            <p>
+              <strong>Actors:</strong> {movie.actors}
+            </p>
+          </div>
+          <div className="ratings">
+            <div className="ratings-score">
+              <p>
+                <span className="metacrtic-score">{metascore}</span> Metascore
+              </p>
+            </div>
+            <div className="ratings-score">
+              <p>
+                <span>
+                  <img
+                    src={rottenTomatoes >= 60 ? '../tomato-icon.png' : '../splat-icon.png'}
+                    alt=""
+                  />
+                </span>
+                {rottenTomatoes}%
+              </p>
+            </div>
+            <div className="ratings-score">
+              <p>
+                <span>
+                  <img src="../imdb-logo.png" alt="" />
+                </span>
+                {imdb}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
