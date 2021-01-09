5f1b62ee65040498678a187af72b940851ae891e
Nicolle Romero <nicollearomero@gmail.com>
2021-01-09 13:58:35 -0800

    Updates to tests

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { act, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 
+// This will mock the IntersectionObserver API.
+import 'react-intersection-observer/test-utils';
+
 import App from '../App';
+
+// Using any because of typing bug in Jest.
+function Wrapper(props: any) {
+  return (
+    <Router>
+      <RecoilRoot>{props.children}</RecoilRoot>
+    </Router>
+  );
+}
+
+function sleep(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 // testing for title
 test('renders title', () => {
-  render(<App />, { wrapper: RecoilRoot });
+  render(<App />, { wrapper: Wrapper });
   const elements = screen.getAllByText(/the shoppies/i);
   expect(elements.length > 0).toBeTruthy();
 });
 
 // testing for footer on load
 test('renders footer', () => {
-  render(<App />, { wrapper: RecoilRoot });
+  render(<App />, { wrapper: Wrapper });
   const elements = screen.getAllByText(/you'd like to nominate to/i);
   expect(elements.length > 0).toBeTruthy();
 });
 
-const sleep = (t: number) => new Promise((resolve) => setTimeout(resolve, t));
-
 // testing end-to-end search
 test('submit search and return movie list', async () => {
-  render(<App />, { wrapper: RecoilRoot });
+  render(<App />, { wrapper: Wrapper });
   userEvent.type(screen.getByRole('textbox'), 'Star Wars');
-  userEvent.click(screen.getByText(/send/i));
+  userEvent.click(screen.getByText(/search/i));
   await sleep(3000);
-  const movieCards = screen.getAllByText(/star\s+wars/i);
-  // console.warn(movieCards.map((el) => el.textContent));
-  expect(movieCards.length).toEqual(10);
-});
-
-// testing nominate movie
-test('submit movie nomination', async () => {
-  render(<App />, { wrapper: RecoilRoot });
-  userEvent.type(screen.getByRole('textbox'), '');
-  userEvent.click(screen.getByText(/send/i));
-  await sleep(3000);
-  // const movieCards = screen.getAllByText(/star\s+wars/i);
-  userEvent.hover(screen.getByTestId('Star Wars: Episode IV - A New Hope'));
-  const nominateButtons = screen.getAllByText(/nominate!/i);
-  userEvent.click(nominateButtons[0]);
-  const nominatedMovie = screen.getAllByTestId(/tt+/i);
-  expect(nominatedMovie.length).toBeGreaterThanOrEqual(1);
-  // await act(() => sleep);
+  const movieCards = screen.getAllByText(/star wars/i);
+  console.warn(movieCards.map((el) => el.textContent));
+  expect(movieCards.length).toBeGreaterThanOrEqual(10);
 });
