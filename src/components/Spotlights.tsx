fb72c175cfeded29db6385035b147346cf14403d
Nicolle Romero <nicollearomero@gmail.com>
2021-01-09 09:08:51 -0800

    Updates to spotlight effect

---
+++ src/components/Spotlights.tsx
@@ -0,0 +1,10 @@
+import './Spotlights.css';
+
+export function Spotlights() {
+  return (
+    <>
+      <div className="spotlight spot1"></div>
+      <div className="spotlight spot2"></div>
+    </>
+  );
+}
