27a6826410b8c0f22bf3cc91412ac025db70c40a
Nicolle Romero <nicollearomero@gmail.com>
2021-01-12 21:19:40 -0800

    Updates to footer scenes

--- src/components/CallToActionBar.tsx
+++ src/components/CallToActionBar.tsx
@@ -1,15 +1,19 @@
+import { MAX_NOMINATIONS } from '../constants';
 import { Sparkles } from './Sparkles';
+
+import statueIcon from '../images/statue-icon.png';
 
 import './CallToActionBar.css';
 
 export function CallToActionBar() {
   return (
     <div className="call-to-action-bar">
-      <img className="statue" src="../statue_icon_green.png" alt="awards statue" />
+      <img className="statue" src={statueIcon} alt="awards statue" />
       <Sparkles>
         <p className="text">
-          Welcome to the home of the Shoppies! Select <span className="number">5</span> movies you'd
-          like to nominate to receive a Shoppies award!
+          Welcome to the home of the Shoppies! Select{' '}
+          <span className="number">{MAX_NOMINATIONS}</span> movies you'd like to nominate to receive
+          a Shoppies award!
         </p>
       </Sparkles>
     </div>
